3e5cc663b8645aa124df2215f1ce6c3f903ea394
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-08-16 21:25:08 -0600

    comments are your friend

--- public/calc.js
+++ public/calc.js
@@ -3,19 +3,22 @@ var config = function() {
   // var buttons = $('<td>');
   // buttons.css('height', '20px');
   // buttons.css('width', '48px');
-
+// Create variables for button objects
   var clearButton = $('#clear');
   var numberButtons = $('.number');
   var operandButtons = $('.operator').not('#clear, #equals, #decimal')
   var decimalButton = $('#decimal')
   var equalsButton = $('#equals')
   var displayScreen = $('#display');
+  // Bump up the input window a bit, makes it less jaring
   displayScreen.css('height', '20px');
 
+  // For the calculations
   var previousNumber = [];
   var currentNumber = [];
   var currentOperator = '';
 
+// To clear the window, very important
   clearButton.click(function(e) {
     console.log('clear');
     displayScreen.text("");
@@ -23,12 +26,14 @@ var config = function() {
     currentNumber = [];
   });
 
+// Events for the buttons
   numberButtons.click(function(e) {
     console.log($(this).attr('id'));
     displayScreen.append($(this).attr('id'));
     currentNumber.push($(this).attr('id'));
   });
 
+// Events for the operations
   operandButtons.click(function(e) {
     console.log($(this).attr('id'));
     previousNumber = currentNumber;
@@ -36,6 +41,7 @@ var config = function() {
     currentOperator = $(this).attr('id');
   });
 
+// Event for "got to get a sum!" Calls the doCalc() method
   equalsButton.click(function(e) {
     console.log('equals');
     displayScreen.text(doCalc());
@@ -43,6 +49,7 @@ var config = function() {
     currentNumber = [];
   });
 
+// Main meat and potatoes
   var doCalc = function() {
     console.log(previousNumber);
     console.log(currentNumber);
